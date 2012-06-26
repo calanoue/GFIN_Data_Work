f74ef877561afc61a2c7bc9af2822cd9eef24845
Chris <calanoue@gmail.com>
2012-06-26 14:19:13 -0400

    adjusted code to insert data into database after each forecasting method for memory issues

--- formatting_forecasting_for_new_student_version.py
+++ formatting_forecasting_for_new_student_version.py
@@ -13,6 +13,7 @@ END_YEAR = 2030 # Last year in the database
 NUM_FORECASTS = 7 # Number of forecast methods
 ID_SLICE = slice(0, VALUE_COLUMN) # Slice for where id columns are located
 X = np.arange(1961, 2031) # Valid years for database
+TABLE = "Datum" # Table to update in the database
 exp_smooth = ExpSmooth() # Exponential smoothing class
 
 # Connect to the database and create a cursor
@@ -75,58 +76,21 @@ for row in datum_xs:
 # Resize the array to remove blank rows of data
 new_datum_xs = np.ma.resize(new_datum_xs, (count, new_datum_xs.shape[1]))
 
-# Extract the value and the id data from the returned query
-values = new_datum_xs[:, VALUE_COLUMN:]
-ids = new_datum_xs[:, ID_SLICE]
-
-# Go through each row of remaining data - except population - and forecast using trend line methods above
-# Add a new column at the end of ids to keep track of the forecasting trend method
-trend_datum_xs = np.ma.masked_all((new_datum_xs.shape[0] * NUM_FORECASTS, new_datum_xs.shape[1] + 1), np.float)
-count = 0
-for enum, value_row in enumerate(values):
-    xs = value_row[~value_row.mask]
-    yrs = X[~value_row.mask]
-    forecast_yrs = np.arange(np.max(yrs) + 1, END_YEAR + 1)
-    forecast_periods = forecast_yrs.shape[0]
-    for forecast_method in xrange(1, NUM_FORECASTS + 1):
-
-        # Forecast one method at a time
-        trend_xs = forecast_from_trend_line(xs, yrs, forecast_yrs, forecast_periods, forecast_method)
-
-        # Add masked values to the start if minimum starting year is greater than the first year
-        min_yr = np.min(yrs)
-        try:
-            trend_datum_xs[count] = np.ma.hstack(
-                (ids[enum], forecast_method, np.ma.masked_all(min_yr - START_YEAR), xs, trend_xs)
-            )
-        except ValueError:
-            print "ValueError", enum, ids[enum], forecast_method, np.ma.masked_all(min_yr - START_YEAR)
-            exit()
-        count += 1
-
-# Countries to keep commodity, GDP, and Population data
-# TODO - change x and z coordinates to -1 for some of the countries or just find their centroids
-
 # Append population and population net change arrays to the formatted and forecasted datum table
+count = 0
 Q = "SELECT * FROM Datum WHERE element_id BETWEEN 511 AND 703"
 pop_xs = np.ma.masked_equal(cursor.execute(Q).fetchall(), -1)[:, 1:]
-pop_xs = np.ma.column_stack((pop_xs[:, ID_SLICE], np.ma.masked_all((pop_xs.shape[0], 1)), pop_xs[:, VALUE_COLUMN:]))
-trend_datum_xs = np.ma.vstack((trend_datum_xs, pop_xs))
-
-# Sort on index columns - forecast, element, item, country
-dtype = ",".join('<f8' for _ in xrange(trend_datum_xs.shape[1]))
-trend_datum_xs = np.ma.sort(trend_datum_xs.view(dtype), order=['f5', 'f2', 'f1', 'f0'], axis=0).view(np.float)
-
-# Add in a primary key field
-trend_datum_xs = np.ma.column_stack((np.ma.arange(1, trend_datum_xs.shape[0] + 1), trend_datum_xs))
-
-# Change missing values to -1 for storage in database
-trend_datum_xs = np.ma.filled(trend_datum_xs, -1)
+pop_xs = np.ma.filled(np.ma.column_stack(
+    (np.ma.arange(
+        count, pop_xs.shape[0]
+    ), pop_xs[:, ID_SLICE], np.ma.masked_all((pop_xs.shape[0], 1)), pop_xs[:, VALUE_COLUMN:])
+), -1)
+count += pop_xs.shape[0]
 
 # Add new column in the datum table for forecasting method values when adding the new trend data
 Q = """
 DROP TABLE Datum;
-CREATE TABLE Datum (id INTEGER PRIMARY KEY AUTOINCREMENT,country_id INTEGER REFERENCES Country,
+CREATE TABLE %s (id INTEGER PRIMARY KEY AUTOINCREMENT,country_id INTEGER REFERENCES Country,
 item_id INTEGER REFERENCES Item,element_id INTEGER REFERENCES Element,unit_id INTEGER REFERENCES Unit,
 source_id INTEGER REFERENCES Source,forecast_id INTEGER REFERENCES Forecast, yr1961 FLOAT,yr1962 FLOAT,yr1963 FLOAT,
 yr1964 FLOAT,yr1965 FLOAT,yr1966 FLOAT,yr1967 FLOAT,yr1968 FLOAT,yr1969 FLOAT,yr1970 FLOAT,yr1971 FLOAT,yr1972 FLOAT,
@@ -137,17 +101,61 @@ yr2000 FLOAT,yr2001 FLOAT,yr2002 FLOAT,yr2003 FLOAT,yr2004 FLOAT,yr2005 FLOAT,yr
 yr2009 FLOAT,yr2010 FLOAT,yr2011 FLOAT,yr2012 FLOAT,yr2013 FLOAT,yr2014 FLOAT,yr2015 FLOAT,yr2016 FLOAT,yr2017 FLOAT,
 yr2018 FLOAT,yr2019 FLOAT,yr2020 FLOAT,yr2021 FLOAT,yr2022 FLOAT,yr2023 FLOAT,yr2024 FLOAT,yr2025 FLOAT,yr2026 FLOAT,
 yr2027 FLOAT,yr2028 FLOAT,yr2029 FLOAT,yr2030 FLOAT);
-"""
+"""%TABLE
 cursor.executescript(Q)
 
-# Insert new data into Datum table
+# Insert population data into Datum table
 cursor.executemany(
-    "INSERT INTO Datum VALUES(%s)"%','.join('?' for _ in xrange(trend_datum_xs.shape[1])), trend_datum_xs
+    "INSERT INTO %s VALUES(%s)"%(TABLE, ','.join('?' for _ in xrange(pop_xs.shape[1]))), pop_xs
 )
 connection.commit()
 
+# Extract the value and the id data from the returned query
+values = new_datum_xs[:, VALUE_COLUMN:]
+ids = new_datum_xs[:, ID_SLICE]
+
+# Go through each row of remaining data - except population - and forecast using trend line methods above
+# Add a new column at the end of ids to keep track of the forecasting trend method
+N = new_datum_xs.shape[0]
+for forecast_method in xrange(1, NUM_FORECASTS + 1):
+    trend_datum_xs = np.ma.masked_all((N, new_datum_xs.shape[1] + 1), np.float)
+    for enum, value_row in enumerate(values):
+        xs = value_row[~value_row.mask]
+        yrs = X[~value_row.mask]
+        forecast_yrs = np.arange(np.max(yrs) + 1, END_YEAR + 1)
+        forecast_periods = forecast_yrs.shape[0]
+
+        # Forecast one method at a time
+        trend_xs = forecast_from_trend_line(xs, yrs, forecast_yrs, forecast_periods, forecast_method)
+
+        # Add masked values to the start if minimum starting year is greater than the first year
+        trend_datum_xs[enum] = np.ma.hstack(
+            (ids[enum], forecast_method, np.ma.masked_all(np.min(yrs) - START_YEAR), xs, trend_xs)
+        )
+
+    # Sort on index columns - forecast, element, item, country
+    dtype = ",".join('<f8' for _ in xrange(trend_datum_xs.shape[1]))
+    trend_datum_xs = np.ma.sort(trend_datum_xs.view(dtype), order=['f5', 'f2', 'f1', 'f0'], axis=0).view(np.float)
+
+    # Add in a primary key field
+    trend_datum_xs = np.ma.column_stack((np.ma.arange(count, count + N), trend_datum_xs))
+
+    # Change missing values to -1 for storage in database
+    trend_datum_xs = np.ma.filled(trend_datum_xs, -1)
+
+    # Insert forecasted data into Datum table
+    cursor.executemany(
+        "INSERT INTO %s VALUES(%s)"%(TABLE, ','.join('?' for _ in xrange(trend_datum_xs.shape[1]))), trend_datum_xs
+    )
+    connection.commit()
+
+    # Increase count of records for primary key index
+    count += N
+
+# TODO - change x and z coordinates to -1 for some of the countries or just find their centroids
+
 # Add index to Datum table
-cursor.execute("CREATE INDEX Datum_index ON Datum (forecast_id, element_id, item_id, country_id)")
+cursor.execute("CREATE INDEX %s_index ON %s (forecast_id, element_id, item_id, country_id)"%(TABLE, TABLE))
 connection.commit()
 
 def forecast_xs_consumption(formatted_xs, id_xs, country_id_index):
