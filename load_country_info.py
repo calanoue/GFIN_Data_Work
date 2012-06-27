aff1c76563e5e25388e5b6ee23e5aa2eb0e22f20
Chris <calanoue@gmail.com>
2012-06-27 18:15:52 -0400

    added a new centroid file for the old x, y coordinates

--- load_country_info.py
+++ load_country_info.py
@@ -6,16 +6,18 @@ import sqlite3
 import numpy as np
 
 # Connect to the database and create a cursor for extracting data
-DB = r".\FBS_ProdSTAT_PriceSTAT_TradeSTAT.db3"
+DB = r"C:\Users\calanoue\Dropbox\Dont Want to Lose\GFIN Random Python Work\demoGFIN\sqlite_student_db.db3"
 connection = sqlite3.connect(DB)
 cursor = connection.cursor()
 
-xs = np.genfromtxt(".\Country_Colors_Centroids.csv",
-    delimiter=",", names=True, usemask=True, dtype=None)
+xs = np.genfromtxt(
+    ".\Country_Colors_Centroids_from_old_database.csv", delimiter=",", names=True, usemask=True, dtype=None
+)
+Q = ""
 for y in xs:
-    x, z, R, G, B, id = y
-    Q = "UPDATE Country SET x=%s, z=%s, R=%s, G=%s, B=%s WHERE country_id=%s"%(x, z, R, G, B, id)
-    cursor.execute(Q)
+    wb_name, x, z, R, G, B, id = y
+    Q += "UPDATE Country SET x=%s, z=%s, R=%s, G=%s, B=%s WHERE country_id=%s;\n"%(x, z, R, G, B, id)
+cursor.executescript(Q)
 connection.commit()
 
 cursor.close()
